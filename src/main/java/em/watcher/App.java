a2f761045dab67a2b731eccaeef329849aa19e17
Sea <hjw_redeption@outlook.com>
2016-03-24 14:20:18 -1200

    first commit

---
+++ src/main/java/em/watcher/App.java
@@ -0,0 +1,23 @@
+package em.watcher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.bind.RelaxedPropertyResolver;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class App {
+
+    @RequestMapping("/")
+    public String home() {
+        return "Hello World";
+    }
+
+
+    public static void main(String[] args) {
+        SpringApplication.run(App.class, args);
+    }
+
+}
